b80c29f85a368f316e445350b5459e944f8e8006
Gabriela Sousa <102119470+GabrielaSousaDev@users.noreply.github.com>
2022-08-26 15:25:53 -0300

    Criado enum

---
+++ src/main/java/br/sc/senac/budgetech/backend/enumeration/Status.java
@@ -0,0 +1,13 @@
+package br.sc.senac.budgetech.backend.enumeration;
+
+public enum Status {
+	IN_SCHEDULE,
+    IN_BUDGET,
+    APPROVED,
+    DISAPPROVED,
+    IN_CUT,
+    IN_PRODUCTION,
+    READY,
+    DELIVERED,
+    LATE;
+}
---
+++ src/main/java/br/sc/senac/budgetech/backend/model/Woodwork.java
@@ -0,0 +1,5 @@
+package br.sc.senac.budgetech.backend.model;
+
+public class Woodwork {
+
+}
